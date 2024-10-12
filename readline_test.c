140052672952cd275ba11079d1178f05bb913b6e
uygarpolat <uygarpolat@gmail.com>
2024-10-12 16:23:09 +0300

    Add initial files to tokenization branch

---
+++ readline_test.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   readline_test.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: upolat <upolat@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/30 11:16:11 by upolat            #+#    #+#             */
+/*   Updated: 2024/09/30 11:16:15 by upolat           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <readline/history.h>
+
+
+void    handle_sigint()
+{
+    write(STDOUT_FILENO, "\n", 1);
+    rl_on_new_line();
+    rl_replace_line("", 0);
+    rl_redisplay();
+}
+
+void    handle_sigquit()
+{
+    rl_on_new_line();
+    rl_replace_line("", 0);
+    rl_redisplay();
+}
+
+int    main(void)
+{
+    char    *input;
+
+    signal(SIGINT, handle_sigint);
+    signal(SIGQUIT, handle_sigquit);
+    while (1)
+    {
+        input = readline("minishell> ");
+        if (!input)
+            break ;
+        if (!strncmp(input, "exit", 5))
+            break ;
+        if (*input)
+            add_history(input);
+        printf("%s\n", input);
+        free(input);
+    }
+    return (0);
+}
