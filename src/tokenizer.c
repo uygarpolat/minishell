d1f8fdc174d23eec891063c1143d19607c475b29
uygarpolat <uygarpolat@gmail.com>
2024-10-18 10:36:21 +0300

    tokenizer improvements

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: upolat <upolat@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 11:16:11 by upolat            #+#    #+#             */
-/*   Updated: 2024/10/17 22:29:20 by upolat           ###   ########.fr       */
+/*   Updated: 2024/10/18 10:35:19 by upolat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,23 +20,22 @@
 #include "../../library/libft/libft.h"
 #include "../includes/tokenizer.h"
 
-
-void    handle_sigint()
+void	handle_sigint(void)
 {
-    write(STDOUT_FILENO, "\n", 1);
-    rl_on_new_line();
-    rl_replace_line("", 0);
-    rl_redisplay();
+	write(STDOUT_FILENO, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
 }
 
-void    handle_sigquit()
+void	handle_sigquit(void)
 {
-    rl_on_new_line();
-    rl_replace_line("", 0);
-    rl_redisplay();
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
 }
 
-void	print_tokens(t_tokens *tokens, t_capacity capacity)
+void	print_tokens(t_tokens *tokens, t_capacity *capacity)
 {
 	char *token_type_str[] = {
 		"TOKEN_WORD",
@@ -53,14 +52,13 @@ void	print_tokens(t_tokens *tokens, t_capacity capacity)
 		"NUM_TYPES"
 	};
 
-	for (int i = 0; i < capacity.current_size; i++)
+	for (int i = 0; i < capacity->current_size; i++)
 	{
 		char formatted_token[100];
 		snprintf(formatted_token, sizeof(formatted_token), "token %d: %s", i, tokens[i].value);
 		printf("%-30s type: %s\n", formatted_token, token_type_str[tokens[i].type]);
 	}
 }
-
 
 int	is_space(char c)
 {
@@ -71,35 +69,57 @@ int	is_space(char c)
 
 int	is_seperator(char c)
 {
-	if (c == '<' || c == '>' || c == '&' || c == '|' ||
-			c == '(' || c == ')')
+	if (c == '<' || c == '>' || c == '&' || c == '|'
+		|| c == '(' || c == ')')
 		return (1);
 	return (0);
 }
-/*
-t_tokens	*ft_realloc_tokens_when_full(t_tokens **tokens, t_capacity *capacity)
+
+void	free_tokens(t_tokens *tokens, t_capacity *capacity)
 {
+	int	i;
+
+	i = 0;
+	while (i < capacity->max_size)
+	{
+		if (i < capacity->current_size)
+			free(tokens[i].value);
+		tokens[i].value = NULL;
+		i++;
+	}
+	free(tokens);
+	tokens = NULL;
+}
+
+t_tokens	*ft_realloc_tokens_when_full(t_tokens *tokens, t_capacity *capacity)
+{
+	int			i;
 	t_tokens	*new_tokens;
 
 	new_tokens = malloc(sizeof(t_tokens) * (capacity->max_size * 2));
 	if (new_tokens == NULL)
 	{
-		free(tokens); // Also free the string stored in array of struct
+		free_tokens(tokens, capacity);
 		return (NULL);
 	}
+	i = -1;
+	while (++i < capacity->current_size)
+	{
+		new_tokens[i].value = ft_strdup(tokens[i].value);
+		new_tokens[i].type = tokens[i].type;
+	}
+	free_tokens(tokens, capacity);
+	capacity->max_size *= 2;
+	return (new_tokens);
 }
-*/
-
 
 void	handle_seperator(char **input, t_tokens *tokens, t_capacity *capacity)
 {
-	char *temp;
-	t_token_type type;
+	char			*temp;
+	t_token_type	type;
 
 	temp = *input;
 	type = TOKEN_UNKNOWN;
-
-
 	if (*temp == '<')
 	{
 		temp++;
@@ -232,30 +252,25 @@ void	handle_word(char **input, t_tokens *tokens, t_capacity *capacity)
 	*input = temp;
 }
 
-t_tokens	*ft_tokenizer(char *input)
+t_tokens	*ft_tokenizer(char *input, t_capacity *capacity)
 {
-	t_capacity	capacity;
 	t_tokens	*tokens;
 
-	capacity.max_size = 20;
-	capacity.current_size = 0;
-	tokens = malloc(sizeof(t_tokens) * capacity.max_size);
+	capacity->max_size = 1;
+	capacity->current_size = 0;
+	tokens = malloc(sizeof(t_tokens) * capacity->max_size);
 	if (tokens == NULL)
 		return (NULL);
 	while (*input)
 	{
 		while (ft_strchr(" \t\n", *input) && *input)
 			input++;
-		if (capacity.max_size <= capacity.current_size)
-		{
-			return (NULL);
-			//ft_realloc_when_tokens_full(&tokens, &capacity);
-		}
+		if (capacity->max_size <= capacity->current_size)
+			tokens = ft_realloc_tokens_when_full(tokens, capacity);
 		if (is_seperator(*input))
-			handle_seperator(&input, tokens, &capacity);
+			handle_seperator(&input, tokens, capacity);
 		else
-			handle_word(&input, tokens, &capacity);
-		//capacity.current_size++;
+			handle_word(&input, tokens, capacity);
 	}
 	print_tokens(tokens, capacity);
 	return (tokens);
@@ -265,6 +280,7 @@ int    main(void)
 {
     char    	*input;
 	t_tokens	*tokens;
+	t_capacity	capacity;
 
     //signal(SIGINT, handle_sigint);
     signal(SIGQUIT, handle_sigquit);
@@ -277,8 +293,8 @@ int    main(void)
             break ;
         if (*input)
             add_history(input);
-		tokens = ft_tokenizer(input);
-		(void)tokens;
+		tokens = ft_tokenizer(input, &capacity);
+		free_tokens(tokens, &capacity);
         free(input);
     }
     return (0);
