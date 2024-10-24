17898a8971224297e472db6f2f1c7e68ff61a207
Uygar Polat <upolat@c1r5p11.hive.fi>
2024-10-24 11:32:11 +0300

    main file refactoring

---
+++ src/main.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: upolat <upolat@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/24 11:17:35 by upolat            #+#    #+#             */
+/*   Updated: 2024/10/24 11:28:45 by upolat           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../library/libft/libft.h"
+#include "../includes/tokenizer.h"
+#include "../includes/parser.h"
+
+void	handle_sigint();
+void	handle_sigquit();
+void	print_ast(t_ast_node *node, int level);
+
+int	main(void)
+{
+	char		*input;
+	t_tokens	*tokens;
+	t_capacity	capacity;
+	t_ast_node	*ast;
+
+	signal(SIGINT, handle_sigint);
+	signal(SIGQUIT, handle_sigquit);
+	while (1)
+	{
+		input = readline("minishell> ");
+		if (!input)
+			break ;
+		if (!ft_strncmp(input, "exit", 5))
+			break ;
+		if (*input)
+			add_history(input);
+		tokens = ft_tokenizer(input, &capacity);
+		ast = build_ast(tokens, 0, capacity.current_size - 1);
+		print_ast(ast, 0);
+		free_tokens(tokens, &capacity);
+		free(input);
+	}
+	return (0);
+}
+
