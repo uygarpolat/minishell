e63f1a7e3ef0eea22cbca283841fd17df8fb1ff9
uygarpolat <uygarpolat@gmail.com>
2024-10-28 23:42:39 +0200

    malloc protections added

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 /*   By: upolat <upolat@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 10:37:29 by upolat            #+#    #+#             */
-/*   Updated: 2024/10/28 17:34:03 by upolat           ###   ########.fr       */
+/*   Updated: 2024/10/28 23:39:46 by upolat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,19 +49,24 @@ int	find_matching_paren(t_tokens *tokens, int start, int end)
 	return (-1);
 }
 
+void *free_void(void **var, void *return_value)
+{
+	if (var && *var)
+	{
+		free(*var);
+		*var = NULL;
+	}
+	return (return_value);
+}
+
 void	free_ast(t_ast *node)
 {
 	if (!node)
 		return ;
 	if (node->token)
 	{
-		if (node->token->value)
-		{
-			free(node->token->value);
-			node->token->value =  NULL;
-		}
-		free(node->token);
-		node->token = NULL;
+		free_void((void**)&node->token->value, NULL);
+		free_void((void**)&node->token, NULL);
 	}
 	if (node->left)
 		free_ast(node->left);
@@ -69,8 +74,7 @@ void	free_ast(t_ast *node)
 		free_ast(node->right);
 	if (node->redir_target)
 		free_ast(node->redir_target);
-	free(node);
-	node = NULL;
+	free_void((void**)&node, NULL);
 }
 
 t_tokens *copy_token(t_tokens *token)
@@ -78,24 +82,17 @@ t_tokens *copy_token(t_tokens *token)
 	t_tokens *new_token;
 
    	new_token = malloc(sizeof(t_tokens));
-	if (!new_token)
+	if (new_token == NULL)
 		return (NULL);
 	new_token->value = ft_strdup(token->value);
 	if (new_token->value == NULL)
-	{
-		free(new_token);
-		new_token = NULL;
-		return (NULL);
-	}
+		return ((t_tokens* )free_void((void**)&new_token, NULL));
 	new_token->type = token->type;
 	return (new_token);
 }
 
-t_ast	*create_node(t_tokens *token)
+void	assign_token_type(t_ast *node, t_tokens *token)
 {
-	t_ast	*node;
-
-	node = malloc(sizeof(t_ast)); // Do malloc check
 	if (token->type == TOKEN_PIPE)
 		node->type = AST_PIPE;
 	else if (token->type == TOKEN_AND)
@@ -112,22 +109,23 @@ t_ast	*create_node(t_tokens *token)
 		node->type = AST_HEREDOC;
 	else if (token->type == TOKEN_WORD)
 		node->type = AST_COMMAND;
+}
+
+t_ast	*create_node(t_tokens *token)
+{
+	t_ast	*node;
+
+	node = malloc(sizeof(t_ast));
+	if (node == NULL)
+		return (NULL);
+	assign_token_type(node, token);
 	node->token = copy_token(token);
+	if (node->token == NULL)
+		return ((t_ast* )free_void((void**)&node, NULL));
 	node->left = NULL;
 	node->right = NULL;
 	node->redir_target = NULL;
 	return (node);
-}
-
-char	*beautify_token(char *str)
-{
-	char	*result;
-
-	(void)result;
-	if (ft_strchrnul(str, '\'') <= ft_strchrnul(str, '"'))
-		return (str);
-	else
-		return (str);
 }
 
 int	redirection_node_creator(t_tokens *tokens, t_ast *root, int *i)
@@ -144,9 +142,10 @@ int	redirection_node_creator(t_tokens *tokens, t_ast *root, int *i)
 	if (new_redir_node == NULL)
 		return (-1);
 	free(new_redir_node->token->value);
+	new_redir_node->token->value = NULL;
 	new_redir_node->token->value = ft_strdup(tokens[++(*i)].value);
 	if (new_redir_node->token->value == NULL)
-		return (free(new_redir_node), -1);
+		return (free_ast(new_redir_node), -1);
 	if (root->redir_target == NULL)
 		root->redir_target = new_redir_node;
 	else
@@ -176,36 +175,36 @@ int	concatenate_commands(char **str, t_tokens *tokens, int *i)
 		if (*str == NULL)
 			return (-1);
 	}
-	(*i)++;
 	return (0);
 }
 
+int	identify_token(t_token_type type)
+{
+	if ((type == TOKEN_REDIR_OUT) || (type == TOKEN_APPEND) || (type == TOKEN_REDIR_IN) || (type == TOKEN_HEREDOC))
+		return (1);
+	return (0);
+
+}
 int	populate_command_node(t_tokens *tokens, t_ast *root, int start, int *end)
 {
 	int		i;
 	char	*str;
 
 	str = NULL;
-	i = start;
+	i = start - 1;
 	root->type = AST_COMMAND;
-	while (i <= *end)
+	while (++i <= *end)
 	{
-		if ((tokens[i].type == TOKEN_REDIR_OUT) || (tokens[i].type == TOKEN_APPEND)
-			|| (tokens[i].type == TOKEN_REDIR_IN) || (tokens[i].type == TOKEN_HEREDOC))
+		if (identify_token(tokens[i].type))
 			redirection_node_creator(tokens, root, &i);
 		else if (concatenate_commands(&str, tokens, &i) == -1)
-			return (-1);
+			return (-1); // Gotta free str as well.
 	}
 	if (str)
 	{
-		if (root->token->value)
-		{
-			free(root->token->value);
-			root->token->value = NULL;
-		}
+		free_void((void**)&root->token->value, NULL);
 		root->token->value = ft_strdup(str);
-		free(str);
-		str = NULL;
+		free_void((void**)&str, NULL);
 	}
 	return (0);
 }
