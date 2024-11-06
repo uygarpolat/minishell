75c508dd7492751957022190fb835705ccf93a00
Uygar Polat <upolat@c1r6p13.hive.fi>
2024-11-06 16:04:13 +0200

    0 implementation

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: upolat <upolat@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 11:17:35 by upolat            #+#    #+#             */
-/*   Updated: 2024/11/06 14:38:54 by upolat           ###   ########.fr       */
+/*   Updated: 2024/11/06 16:02:53 by upolat           ###   ########.fr       */
 /*   Updated: 2024/10/28 17:20:14 by hpirkola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
@@ -53,12 +53,12 @@ int	main(int argc, char **argv, char **envp)
 			break ;
 		if (*input)
 			add_history(input);
-		tokens = ft_tokenizer(input, &capacity, new_envp);
+		tokens = ft_tokenizer(input, &capacity, new_envp, code);
 		if (tokens)
 		{
 			ast = build_ast(tokens, 0, capacity.current_size - 1, 0);
 			//if (ast)
-				//print_ast(ast, 0);		
+			//	print_ast(ast, 0);		
 		}
 		else
 			continue ;
