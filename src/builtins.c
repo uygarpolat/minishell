bec8fee34fb0c656027054570e3872d9a75a5bc5
Helmi  Pirkola <hpirkola@c1r5p9.hive.fi>
2024-11-13 17:49:04 +0200

    fixed error messages and exiting after echo

--- src/builtins.c
+++ src/builtins.c
@@ -6,7 +6,7 @@
 /*   By: hpirkola <hpirkola@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 14:02:47 by hpirkola          #+#    #+#             */
-/*   Updated: 2024/11/11 19:24:10 by hpirkola         ###   ########.fr       */
+/*   Updated: 2024/11/13 17:19:38 by hpirkola         ###   ########.fr       */
 /*   Updated: 2024/10/29 10:29:40 by upolat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
@@ -129,112 +129,135 @@ int	execute_builtin(t_ast *s, char **cmd, char ***envp, t_minishell *minishell, 
 	file.infile = NULL;
 	file.outfile = NULL;
 	if (minishell->p.count == 0)
+		minishell->p.pids[0] = fork();
+	if (minishell->p.pids[0] == 0 || minishell->p.count > 0)
 	{
-		get_in_out(s, &file, minishell);
-		if (file.infile || file.outfile)
-			dupping(minishell, &minishell->p, &file, n);
-	}
-	if (!ft_strncmp(cmd[0], "echo", 5))
-	{
-		if (cmd[2] && !ft_strncmp(cmd[1], "-n", 3))
+		if (minishell->p.count == 0)
 		{
-			i = 1;
-			while (cmd[++i])
+			get_in_out(s, &file, minishell);
+			if (file.infile || file.outfile)
+				dupping(minishell, &minishell->p, &file, n);
+			if (file.infile)
+				close(file.in);
+			if (file.outfile)
+				close(file.out);
+		}
+		if (!ft_strncmp(cmd[0], "echo", 5))
+		{
+			if (!cmd[1])
 			{
-				if (i > 2)
-					printf(" ");
-				printf("%s", cmd[i]);
+				printf("\n");
+				return (1);
+			}
+			if (cmd[2] && !ft_strncmp(cmd[1], "-n", 3))
+			{
+				i = 1;
+				while (cmd[++i])
+				{
+					if (i > 2)
+						printf(" ");
+					printf("%s", cmd[i]);
+				}
+			}
+			else
+			{
+				i = 0;
+				while (cmd[++i])
+				{
+					if (i > 1)
+						printf(" ");
+					printf("%s", cmd[i]);
+				}
+				printf("\n");
 			}
 		}
-		else
+		else if (!ft_strncmp(cmd[0], "cd", 3))
 		{
-			i = 0;
-			while (cmd[++i])
+			if (!cmd[1])
 			{
-				if (i > 1)
-					printf(" ");
-				printf("%s", cmd[i]);
+				//change to home directory
+				exit (0);
 			}
-			printf("\n");
+			if (cmd[2])
+			{
+				ft_putstr_fd(" too many arguments\n", 2);
+				exit (1);
+			}
+			if (chdir(cmd[1]) != 0)
+			{
+				ft_putstr_fd(" No such file or directory\n", 2);
+				exit (1);
+			}
+			*envp = ch_envp(*envp, cmd[1]);
+			if (!envp)
+				exit (1);
+			if (getcwd(minishell->pwd, sizeof(minishell->pwd) != 0))
+			{
+				perror("getcwd error\n");
+				exit (1);
+			}
 		}
-	}
-	else if (!ft_strncmp(cmd[0], "cd", 3))
-	{
-		if (cmd[2])
+		else if (!ft_strncmp(cmd[0], "pwd", 4))
+			printf("%s\n", minishell->pwd);
+		else if (!ft_strncmp(cmd[0], "export", 7))
 		{
-			ft_putstr_fd(" too many arguments\n", 2);
-			return (0);
-		}
-		if (chdir(cmd[1]) != 0)
-		{
-			ft_putstr_fd(" No such file or directory\n", 2);
-			return (0);
-		}
-		*envp = ch_envp(*envp, cmd[1]);
-		if (!envp)
-			return (0);
-		if (getcwd(minishell->pwd, sizeof(minishell->pwd) != 0))
-		{
-			perror("getcwd error\n");
-			return (0);
-		}
-	}
-	else if (!ft_strncmp(cmd[0], "pwd", 4))
-		printf("%s\n", minishell->pwd);
-	else if (!ft_strncmp(cmd[0], "export", 7))
-	{
-		if (cmd[1][0] != '_' && !ft_isalpha(cmd[1][0]))
-		{
-			ft_putstr_fd(" not a valid identifier\n", 2);
-			return (0);
-		}
-		str = ft_split(cmd[1], '=');
-		if (!str)
-		{
-			ft_putstr_fd(" not a valid identifier\n", 2);
-			return (0);
-		}
-		i = -1;
-		while (str[0][++i])
-		{
-			if (str[0][i] != '_' && !ft_isalpha(str[0][i]) && !ft_isdigit(str[0][i]))
+			if (cmd[1][0] != '_' && !ft_isalpha(cmd[1][0]))
 			{
 				ft_putstr_fd(" not a valid identifier\n", 2);
-				return (0);
+				exit (1);
 			}
+			str = ft_split(cmd[1], '=');
+			if (!str)
+			{
+				ft_putstr_fd(" not a valid identifier\n", 2);
+				exit (1);
+			}
+			i = -1;
+			while (str[0][++i])
+			{
+				if (str[0][i] != '_' && !ft_isalpha(str[0][i]) && !ft_isdigit(str[0][i]))
+				{
+					ft_putstr_fd(" not a valid identifier\n", 2);
+					exit (1);
+				}
+			}
+			*envp = add_env(*envp, cmd[1]);
+			if (!envp)
+				exit (1);
 		}
-		*envp = add_env(*envp, cmd[1]);
-		if (!envp)
-			return (0);
-	}
-	else if (!ft_strncmp(cmd[0], "unset", 6))
-	{
-		if (cmd[1])
-			*envp = rm_envp(*envp, cmd[1]);
-		if (!envp)
-			return (0);
-	}
-	else if (!ft_strncmp(cmd[0], "env", 4))
-		print_env(*envp);
-	else if (!ft_strncmp(cmd[0], "exit", 5))
-	{
-		if (cmd[2])
+		else if (!ft_strncmp(cmd[0], "unset", 6))
 		{
-			ft_putstr_fd(" too many arguments\n", 2);
-			return (0);
+			if (cmd[1])
+				*envp = rm_envp(*envp, cmd[1]);
+			if (!envp)
+				exit (1);
 		}
-		i = ft_atoi(cmd[1]);
-		if (i == 0 && ft_strncmp(cmd[1], "0", 2) && ft_strncmp(cmd[1], "+0", 3) && ft_strncmp(cmd[1], "-0", 3))
+		else if (!ft_strncmp(cmd[0], "env", 4))
+			print_env(*envp);
+		else if (!ft_strncmp(cmd[0], "exit", 5))
 		{
-			ft_putstr_fd(" numeric argument required\n", 2);
-			//return (0);
-			error(minishell);
-			exit(2);
+			if (cmd[2])
+			{
+				ft_putstr_fd(" too many arguments\n", 2);
+				exit (1);
+			}
+			i = ft_atoi(cmd[1]);
+			if (i == 0 && ft_strncmp(cmd[1], "0", 2) && ft_strncmp(cmd[1], "+0", 3) && ft_strncmp(cmd[1], "-0", 3))
+			{
+				ft_putstr_fd(" numeric argument required\n", 2);
+				//return (0);
+				error(minishell);
+				exit(2);
+			}
+			else
+				exit(i);
 		}
-		else
-			exit(i);
+		exit(0);
 	}
-	return (1);
+	else if (minishell->p.pids < 0)
+		exit(1);
+	else
+		return (1);
 }
 
 int	is_builtin(char **cmd)
