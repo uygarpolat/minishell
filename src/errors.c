fefe19e79dbf34ad224e07b957eca4806b77c9be
Helmi  Pirkola <hpirkola@c1r4p5.hive.fi>
2025-01-16 14:25:55 +0200

    fixed more leaks

--- src/errors.c
+++ src/errors.c
@@ -6,7 +6,7 @@
 /*   By: upolat <upolat@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 11:05:16 by hpirkola          #+#    #+#             */
-/*   Updated: 2025/01/16 12:40:51 by hpirkola         ###   ########.fr       */
+/*   Updated: 2025/01/16 14:21:41 by hpirkola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,10 @@ void	error(t_minishell *minishell, t_put *cmd, char ***envp)
 		close_and_free(&minishell->p, cmd);
 	if (minishell->p.pids)
 		free(minishell->p.pids);
-	//if (minishell->ast)
-		//free_ast(&minishell->ast);
+	if (minishell->ast)
+		free_ast(&minishell->ast);
 	if (minishell->tokens)
-	{
 		free_tokens(&minishell->tokens, &minishell->capacity);
-		minishell->tokens = NULL;
-	}
 }
 
 void	error2(t_minishell *minishell, char *str, t_put *cmd)
