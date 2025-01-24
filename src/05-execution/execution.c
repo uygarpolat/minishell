908c1d63e4d652b1ec81cf2ed10f2114c2ed2cb5
uygarpolat <uygarpolat@gmail.com>
2025-01-24 13:25:35 +0200

    execution.c norme fixes

--- src/05-execution/execution.c
+++ src/05-execution/execution.c
@@ -6,44 +6,12 @@
 /*   By: upolat <upolat@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 14:14:33 by hpirkola          #+#    #+#             */
-/*   Updated: 2025/01/23 19:01:19 by hpirkola         ###   ########.fr       */
+/*   Updated: 2025/01/24 13:20:50 by upolat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/ast.h"
 #include "../../includes/signals.h"
-
-void	pipe_fail(t_pipes *p)
-{
-	ft_putstr_fd("Pipe failed\n", 2);
-	if (p->count > 1)
-		pipe_close(p, 1, 0);
-}
-
-int	ft_pipe(t_pipes *p, int n, t_minishell *minishell, t_put *cmd)
-{
-	if (n % 2 == 0 && n < p->count && p->count > 0)
-	{
-		if (n > 0)
-			pipe_close(p, 0, 0);
-		if (pipe(p->pipes[0]) < 0)
-			return (error(minishell, cmd), 0);
-		if (n > 0)
-			close(p->pipes[1][1]);
-	}
-	else if (n < p->count && p->count > 0)
-	{
-		if (n > 1)
-			pipe_close(p, 1, 0);
-		if (pipe(p->pipes[1]) < 0)
-		{
-			error(minishell, cmd);
-			return (pipe_close(p, 0, 1), 0);
-		}
-		close(p->pipes[0][1]);
-	}
-	return (1);
-}
 
 void	execute(t_ast *s, t_minishell *minishell, int n, t_put *cmd)
 {
@@ -136,18 +104,6 @@ int	initialize(t_put *cmd, t_minishell *minishell,
 	if (!cmd->cmd_fd)
 		return (0);
 	return (1);
-}
-
-void	unlink_here(t_put *cmd)
-{
-	int	i;
-
-	i = -1;
-	while (++i < 16)
-	{
-		if (access(cmd->heredocs[i], F_OK) == 0)
-			unlink(cmd->heredocs[i]);
-	}
 }
 
 int	execution(t_ast *s, t_token_info *token_info, t_minishell *minishell)
